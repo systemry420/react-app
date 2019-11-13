53f8cd6ea32ae49726e9c7db31836bd7e535fbba
hsn <systemry420gmail.com>
2019-11-13 13:31:30 +0200

    add first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return ( 
+            <div>
+                <h2>Counter</h2>
+                <button>Count</button>
+            </div>
+         );
+    }
+}
+ 
+export default Counter;
